5bb7021b7023815c047dca831a95d331b25f6b31
OmitNomis <simon98190@gmail.com>
2022-09-12 17:00:34 +0545

    creating and storing images in new folder for every generation

--- scrape.py
+++ scrape.py
@@ -1,12 +1,17 @@
 import requests
+import os
+from datetime import datetime
 
-iterations = int(input("How many images do you want to scrape? "))
+folder = datetime.now().strftime('%Y%m%d%H%M%S')
+iterations = int(input("\n\tHow many images do you want to scrape? "))
+os.mkdir(folder)
 
-
+print ("\n\tDirectory created with name {}".format(folder))
 def download(fileName):
-    file = open(fileName,'wb')
+    file = open(os.path.join(folder, fileName),'wb')
     file.write(requests.get('https://thispersondoesnotexist.com/image', headers={'User-Agent': 'My User Agent 1.0'}).content)
     file.close()
+    print ("\n\t{} created".format(fileName))
 
 for i in range(iterations):
-    download(str(i)+'.jpg')
+     download(str(i)+'.jpg')
