1027708bd6f9526326dbb4aee9347158efab8562
OmitNomis <simon98190@gmail.com>
2022-09-12 15:30:56 +0545

    number of iterations

--- scrape.py
+++ scrape.py
@@ -1,9 +1,12 @@
 import requests
 
+iterations = int(input("How many images do you want to scrape? "))
+
+
 def download(fileName):
     file = open(fileName,'wb')
     file.write(requests.get('https://thispersondoesnotexist.com/image', headers={'User-Agent': 'My User Agent 1.0'}).content)
     file.close()
 
-for i in range(30):
+for i in range(iterations):
     download(str(i)+'.jpg')
