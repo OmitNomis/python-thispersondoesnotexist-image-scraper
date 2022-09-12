a32977e25550a219f0d74674b7138a01ce20a509
OmitNomis <simon98190@gmail.com>
2022-09-12 17:07:17 +0545

    formatting

--- scrape.py
+++ scrape.py
@@ -3,10 +3,10 @@ import os
 from datetime import datetime
 
 folder = datetime.now().strftime('%Y%m%d%H%M%S')
-iterations = int(input("\n\tHow many images do you want to scrape? "))
+iterations = int(input("\nHow many images do you want to scrape? "))
 os.mkdir(folder)
 
-print ("\n\tDirectory created with name {}".format(folder))
+print ("\nDirectory created with name {}".format(folder))
 def download(fileName):
     file = open(os.path.join(folder, fileName),'wb')
     file.write(requests.get('https://thispersondoesnotexist.com/image', headers={'User-Agent': 'My User Agent 1.0'}).content)
@@ -14,4 +14,7 @@ def download(fileName):
     print ("\n\t{} created".format(fileName))
 
 for i in range(iterations):
-     download(str(i)+'.jpg')
+     download(str(i+1)+'.jpg')
+
+print ("\n{} images successfully generated!".format(iterations))
+input("\nPress any key to exit...")
